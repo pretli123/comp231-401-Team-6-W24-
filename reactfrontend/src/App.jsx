8103c43201d969c2ff0082f6b560bcd769006088
ajjawi_amer@hotmail.com <ajjawi_amer@hotmail.com>
2024-03-30 19:57:54 -0400

    added banner top

--- reactfrontend/src/App.jsx
+++ reactfrontend/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 import Signup from './components/Signup';
+import banner from './assets/banner.jpg';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="top">
+        <div id="logo">
+          <img src={banner} alt="blue banner" />
+        </div>
+      </div>
+
        <div>
-      <h1>User Signup</h1>
+      <h1>Loop</h1>
       <Signup />
     </div>
 
